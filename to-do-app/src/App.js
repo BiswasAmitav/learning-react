c0b358b089a66b92dcc3e7fb41bdbd183e17b61d
BiswasAmitav <biswasamit2648@gmail.com>
2022-05-11 16:25:26 +0530

    by this code i learnt how to take user input and how it can be used in different diferent part

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+  import {useState} from 'react';
+
+
+
+
+
+
 
 function App() {
+
+  const [todoTitle, setTodoTitle]=useState("")//FOR GETTING THE VALUE FORM USERS.....
+  // console.log(todoTitle);
+
+
+  //TO ADDING USERS VALUE FOR CREATING LIST AFTER CLICKING ADD TO DO....
+  const [todoList,setTodoList]=useState([]);
+
+
+  const createTodoHandler =()=>{
+    if(todoTitle!==''){
+     const newTodo={
+       id:Date.now(),
+       title:todoTitle,
+      isComplete:false
+     };
+
+     setTodoList([...todoList,newTodo]);
+     setTodoTitle("");
+
+    }
+
+    else{
+       //USER CAN BE CLICKED ADD TO WITH EMPTY TITLE...
+      alert("Please Enter a valid title..!");
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-app">
+        <input type="text" value = {todoTitle} onChange={(event)=> setTodoTitle(event.target.value)}></input>
+        <button onClick={createTodoHandler}>
+          Add todo
+        </button>
+        <ul className="todo-list">
+          {todoList.map(todo =>(
+            <li>
+              <span>{todo.title}</span>
+              <button>
+                Edit
+              </button>
+              <button>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }

7773c7d73d538f99ad3e13c4524f036d263570c3
BiswasAmitav <biswasamit2648@gmail.com>
2022-08-15 16:48:58 +0530

    some comment i added for better under standing

--- third-day/src/App.js
+++ third-day/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(100);
 
-const [counter,setCounter]=useState(100);
+  const increase = (data) => {
+    // let temp=counter+1;//here we can not reassign the value...
+    setCounter(counter + data);
+  };
 
-
-const increase=(data)=>{
-  // let temp=counter+1;
-  setCounter(counter+data);
-}
-
-
-
-const decrease=(data)=>{
-  setCounter(counter-data)
-}
+  const decrease = (data) => {
+    setCounter(counter - data);
+  };
 
   //THIS IS THE LOGIC FOR INCREMENT OR DECREMENT ..........
   // let counter=0;
@@ -27,7 +21,6 @@ const decrease=(data)=>{
   //   console.log(counter);
   // }
 
-
   // const decrease=()=>{
   //   counter--;
   //   console.log(counter);
@@ -35,52 +28,46 @@ const decrease=(data)=>{
 
   return (
     <div className="App">
-     <h1>The value of the counter is: {counter}</h1>
-     <hr/>
-
-
+      <h1> The value of the counter is: {counter} </h1> <hr />
       {/* ITS A SIMPL FUNCTION FOR ONE TYPE BUTTON... */}
-
       {/* <button onClick={increase}>
-       Increase By 100
-     </button>
+             Increase By 100
+           </button>
 
 
 
-     <button onClick={decrease}>
-       Decrease By ONe 
-     </button>  */}
+           <button onClick={decrease}>
+             Decrease By ONe 
+           </button>  */}
+      <button onClick={() => increase(1)}>
+        Increase By One 
+      </button>
+
+      <button onClick={() => increase(5)}>
+        Increase By 5 
+      </button>
 
 
-
-     <button onClick={()=>increase(1)}>
-       Increase By One 
-     </button>
-
-     
-     <button onClick={()=>increase(5)}>
-       Increase By 5
-     </button>
-
-     
-     <button onClick={()=>increase(100)}>
-       Increase By 100
-     </button>
+      <button onClick={() => increase(100)}>
+        Increase By 100 
+      </button>
 
 
+      <button onClick={() => decrease(1)}>
+        Decrease By ONe
+      </button>
 
-     <button onClick={()=>decrease(1)}>
-       Decrease By ONe 
-     </button>
+
+      <button onClick={() => decrease(5)}>
+        Decrease By 5 
+      </button>
 
 
-     <button onClick={()=>decrease(5)}>
-       Decrease By 5
-     </button>
+      <button onClick={() => decrease(100)}>
+        Decrease By 100 
+      </button>{" "}
 
-     <button onClick={()=>decrease(100)}>
-       Decrease By 100 
-     </button>
+
     </div>
   );
 }

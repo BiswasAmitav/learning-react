ee38e7644e45bbef1ab6ca5b3a4286a75cbb5605
BiswasAmitav <biswasamit2648@gmail.com>
2022-09-09 21:14:58 +0530

    some of the functionality i've added

--- ultra-nation/src/App.js
+++ ultra-nation/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries,setCountries]=useState([]);
+  //CONCEPT OF DESTRUCTION
+  // const [x,y]=['abc','def'];
+  // const total = add(2,3);
+
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data =>{
+      setCountries(data);
+      console.log(data);
+      // const names=data.map(country => country.name);
+      // console.log(names);
+    })
+    .catch(error =>console.log(error))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{countries.length} Country is loaded . </h1>
+      <ul>
+        {/* {
+          countries.map(country => country.;
+        } */}
+      </ul>
     </div>
   );
 }
+
+
 
 export default App;

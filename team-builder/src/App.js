1fce8d7ec7b77b6f6a634fa6f3f9e101f6cee293
BiswasAmitav <biswasamit2648@gmail.com>
2022-09-22 22:12:52 +0530

    main app file is updated with this commit rendered user component

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {useEffect} from 'react';
+import User  from './components/User/User';
 
 function App() {
+  const [users,setUsers]=useState([]);
+  console.log(users);
+  useEffect(()=>{
+    fetch('https://randomuser.me/api/?results=2')
+    .then((res) => res.json())
+    .then((data) => setUsers(data.results))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Team Builder</h1>
+        {
+          users.map(user => <User user={user}></User>)
+        }
     </div>
-  );
+  )
 }
 
 export default App;
